e93402d73ef438dec53f2e6e9da172506533a6aa
andrelimaiftm <andrelima@iftm.edu.br>
2022-09-19 14:22:27 -0300

    Povoamento do banco de dados

--- backend/src/main/java/com/devsuperior/dscatalog/servicos/CategoriaService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/servicos/CategoriaService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.dscatalog.entidades.Categoria;
 import com.devsuperior.dscatalog.repositorios.CategoriaRepository;
@@ -14,6 +15,7 @@ public class CategoriaService {
 	@Autowired
 	private CategoriaRepository repositorio;
 	
+	@Transactional(readOnly = true)
 	public List<Categoria> buscaTodos(){
 		return repositorio.findAll();
 	}
